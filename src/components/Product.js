2d2f5ad792fb02580fdfb2f0a9171d2d54abeac6
suru4612 <72142498+suru4612@users.noreply.github.com>
2023-03-10 22:42:05 +0530

    Add files via upload

---
+++ src/components/Product.js
@@ -0,0 +1,16 @@
+// import ProductItem from './ProductItem'
+// import './Product.css'
+
+//     function Products(props){
+//         return 
+//         <div className='products'>
+//             {props.product.map((product) => ( 
+//                 <ProductItem name={product.name} 
+//                 type={product.type} 
+//                 price={product.price}/>
+//                 ))
+//         }
+//         </div>
+//     }
+
+// export default Products;
---
+++ src/components/ProductItem.js
@@ -0,0 +1,24 @@
+// import React, {useState} from 'react'
+// import './ProductItem.css'
+
+// const ProductItem = (props) => {
+//     const [changeText, setText] = useState(props.type)
+
+//     function clickHandler (){
+//         setText('Added to Cart')
+//     }
+
+//   return (
+//      <div className='product-item'>
+//     <div>{props.name}</div> 
+//     <div className='product-item__type'>
+//         <p>{props.type}</p>
+//     </div>
+//     <div className='product-item__price'>{props.price}</div>
+//     <button onClick= {clickHandler}>Add to Cart</button>
+
+// </div>
+//   )
+// }
+
+// export default ProductItem
